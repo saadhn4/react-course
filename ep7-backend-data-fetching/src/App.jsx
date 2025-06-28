c53a079626e0b34d949bbfc63b9873e70a9fe5e1
Saad Hussain <saadcfi4@gmail.com>
2025-06-28 08:58:15 +0400

    done till dynamic html generation of cart items in checkout 52:00

---
+++ ep7-backend-data-fetching/src/App.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route } from "react-router";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import CheckoutPage from "./pages/checkout/CheckoutPage";
+import OrdersPage from "./pages/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
+import { NotFound } from "./pages/NotFound";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/cart-items?expand=product").then((response) => {
+      setCart(response.data);
+    });
+  }, []);
+  return (
+    <Routes>
+      {/* same as path="/" */}
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/tracking" element={<TrackingPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ ep7-backend-data-fetching/src/components/Header.jsx
@@ -0,0 +1,49 @@
+import "./Header.css";
+import { NavLink } from "react-router";
+import logo from "../assets/images/logo.png";
+import logoWhite from "../assets/images/logo-white.png";
+import mobileLogoWhite from "../assets/images/mobile-logo-white.png";
+import cartIcon from "../assets/images/icons/cart-icon.png";
+import searchIcon from "../assets/images/icons/search-icon.png";
+
+const Header = ({ cart }) => {
+  let totalQuantity = 0;
+
+  cart.forEach((item) => {
+    totalQuantity += item.quantity;
+  });
+  return (
+    <>
+      <div className="header">
+        <div className="left-section">
+          <NavLink to="/" className="header-link">
+            <img className="logo" src={logoWhite} />
+            <img className="mobile-logo" src={mobileLogoWhite} />
+          </NavLink>
+        </div>
+
+        <div className="middle-section">
+          <input className="search-bar" type="text" placeholder="Search" />
+
+          <button className="search-button">
+            <img className="search-icon" src={searchIcon} />
+          </button>
+        </div>
+
+        <div className="right-section">
+          <NavLink className="orders-link header-link" to="/orders">
+            <span className="orders-text">Orders</span>
+          </NavLink>
+
+          <NavLink className="cart-link header-link" to="/checkout">
+            <img className="cart-icon" src={cartIcon} />
+            <div className="cart-quantity">{totalQuantity}</div>
+            <div className="cart-text">Cart</div>
+          </NavLink>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
